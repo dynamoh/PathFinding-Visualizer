0d719b0f1d86f16f2c64f701b627087d5f4e4dd4
amanchande <2018312@iiitdmj.ac.in>
2021-01-31 14:18:05 +0530

    neumorphic style to nodes

--- src/CreateGraph/CreateGraph.js
+++ src/CreateGraph/CreateGraph.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './create-graph.css'
-import {Form, Radio, Segment} from 'semantic-ui-react'
-import {Graph} from '@dagrejs/graphlib';
+import { Form, Radio, Popup } from 'semantic-ui-react'
+import { Graph } from '@dagrejs/graphlib';
 import $ from 'jquery';
 
 export class CreateGraph extends Component {
@@ -21,8 +21,8 @@ export class CreateGraph extends Component {
     getInitialNodes = () => {
         let nodes = [];
         for (let row = 0; row < 25; row++) {
-        let curr = [];
-            for (let col = 0; col < 60; col++) {
+            let curr = [];
+            for (let col = 0; col < 49; col++) {
                 curr.push(this.createNode(row, col))
             }
             nodes.push(curr)
@@ -32,18 +32,19 @@ export class CreateGraph extends Component {
 
     componentDidMount() {
         const nodes = this.getInitialNodes();
-        this.setState({nodes});
-    }
-    
-    randomIntFromInterval = (min,max) => {
-        return Math.floor(Math.random()*(max-min+1) + min)
+        this.setState({ nodes });
     }
 
-    createNode(row,col) {
+    randomIntFromInterval = (min, max) => {
+        let rand_val = Math.floor(Math.random() * (max - min + 1) + min)
+        return (rand_val === 0 ? 10 : rand_val)
+    }
+
+    createNode(row, col) {
         var newNode = {
-            row:row,
-            col:col,
-            val:0,
+            row: row,
+            col: col,
+            val: 0,
         }
         // var nodes = this.state.node;
         // nodes.push(nodes);
@@ -52,22 +53,22 @@ export class CreateGraph extends Component {
         // });
         return newNode
     }
-    
+
     toggleAddNode() {
         this.setState(
-           (prevState) => ({
-               addNode: !prevState.addNode
-           
-           })
+            (prevState) => ({
+                addNode: !prevState.addNode
+
+            })
         )
     }
 
     handleValueChange(e, node) {
         let nodes = this.state.nodes
-        if (e.target.value > 99 || e.target.value < -99)
-            return ;
+        if (e.target.value > 99 || e.target.value < -99 || e.target.value === 0)
+            return;
         nodes[node.row][node.col].val = e.target.value
-        this.setState({nodes})
+        this.setState({ nodes })
     }
 
     handleClick(val) {
@@ -75,73 +76,61 @@ export class CreateGraph extends Component {
         let nodes = this.state.nodes
         let graph = this.state.graph
         let fromNode = this.state.fromNode
-        let toNode = this.state.toNode
-        const inputElement = document.getElementById("inp-"+val);
+        let fromNode_tmp = fromNode
         const nodeElement = document.getElementById(val);
 
         if (this.state.value === 'addNode') {
             if (nodeElement.className === 'graph-node graph-node-visible')
-                return ;
+                return;
             graph.setNode(val, val);
             nodes[row][col].val = this.randomIntFromInterval(-99, 99)
-            this.setState({nodes})
+            this.setState({ nodes })
             nodeElement.className += " graph-node-visible";
-            inputElement.disabled = false;
         }
+
         else if (this.state.value === 'delNode') {
             fromNode = ''
-            this.setState({fromNode})
+            this.setState({ fromNode })
             graph.removeNode(val)
             nodes[row][col].val = 0
-            inputElement.disabled=true
-            nodeElement.className -= " graph-node-visible";    
+            nodeElement.className -= " graph-node-visible";
         }
+
         else if (this.state.value === 'addEdge') {
             const graphArea = document.getElementsByClassName("draw-graph")[0]
-            graphArea.style.cursor = "pointer";
-            inputElement.style.color = "red";
             if (graph._nodeCount <= 1) {
                 alert('Atleast 2 nodes are needed to create an edge')
-                return ;
+                return;
             }
-            if (!graph.hasNode(val)) 
-                return ;
+            if (!graph.hasNode(val))
+                return;
             if (fromNode !== '') {
-                nodeElement.style.opacity=1;
                 graph.setEdge(fromNode, val);
+                // nodeElement.style.backgroundColor='green';
+                // setTimeout(() => {
+                //     document.getElementById(fromNode_tmp).style.animation = 'bg-fade-anim 2s ease-in';
+                //     nodeElement.style.animation=' bg-fade-anim 2s ease-in';
+                //     document.getElementById(fromNode_tmp).style.backgroundColor = 'rgb(55, 212, 55)';
+                //     nodeElement.style.backgroundColor=' rgb(55, 212, 55)';
+                // }, 2000);
                 fromNode = ''
-                console.log(graph.edges())
-                this.setState({fromNode})
+                this.setState({ fromNode })
             }
             else {
-                nodeElement.style.opacity=1;
                 fromNode = val
-                this.setState({fromNode})
-                console.log(fromNode)
+                this.setState({ fromNode })
+                // nodeElement.style.backgroundColor='green';
             }
 
-        }   
-        else 
-            return ;
+        }
+        else
+            return;
     }
 
     handleChange = (e, { value }) => {
-        let element = $('.graph-node-visible');
-        if (value === 'addEdge') {
-            for(let i=0; i<element.length; i++) {
-                setTimeout(() => {
-                    element[i].style.opacity = 0.65;
-                }, 100 * i);
-            }
-
-        }
-        else {
-            for(let i=0; i<element.length; i++) {
-                element[i].style.opacity = 1;
-            }
-        }
         this.setState({ value })
     }
+
     addNode(e) {
         console.log(e.clientX, e.clientY);
     }
@@ -159,20 +148,18 @@ export class CreateGraph extends Component {
                                     label='Add Nodes'
                                     name='radioGroup'
                                     value='addNode'
-
-                                    checked={this.state.value === 'addNode'}
                                     onChange={this.handleChange}
-                                />    
+                                    checked={this.state.value === 'addNode'}
+                                />
                             </Form.Field>
                             <Form.Field>
                                 <Radio slider
                                     label='Delete Node'
                                     name='radioGroup'
                                     value='delNode'
-
-                                    checked={this.state.value === 'delNode'}
                                     onChange={this.handleChange}
-                                />    
+                                    checked={this.state.value === 'delNode'}
+                                />
                             </Form.Field>
                             <Form.Field>
                                 <Radio slider
@@ -181,7 +168,7 @@ export class CreateGraph extends Component {
                                     value='addEdge'
                                     checked={this.state.value === 'addEdge'}
                                     onChange={this.handleChange}
-                                />    
+                                />
                             </Form.Field>
                             <Form.Field>
                                 <Radio slider
@@ -190,55 +177,65 @@ export class CreateGraph extends Component {
                                     value='animate'
                                     checked={this.state.value === 'animate'}
                                     onChange={this.handleChange}
-                                />    
+                                />
                             </Form.Field>
                         </div>
                     </Form>
                     <div onClick={(e) => this.addNode(e)} className="draw-graph display-flex">
                         <tbody>
-                        {nodes.map(node => {
-                            return (
-                                <tr>
-                                    {node.map(n => {
-                                        return (
-                                            <td
-                                                id={`${n.row}-${n.col}`}
-                                                className="graph-node"
-                                                onClick={() => this.handleClick(`${n.row}-${n.col}`)}
-                                            >
-                                                <input 
-                                                    id={`inp-${n.row}-${n.col}`} 
-                                                    className="graph-node-input" 
-                                                    type="text" 
-                                                    disabled="true" 
-                                                    value={n.val === 0 ? '' : n.val}
-                                                    onChange={(e) => this.handleValueChange(e, n)}
+                            {nodes.map(node => {
+                                return (
+                                    <tr>
+                                        {node.map(n => {
+                                            return (
+                                                <Popup
+                                                    on="click"
+                                                    positionFixed
+                                                    position='bottom'
+                                                    trigger={
+                                                        <td
+                                                            id={`${n.row}-${n.col}`}
+                                                            className={n.val === 0 ? 'graph-node' : 'graph-node graph-node-visible'}
+                                                            onClick={() => this.handleClick(`${n.row}-${n.col}`)}
+                                                        >
+                                                            {n.val === 0 ? '' : n.val}  
+                                                        </td>
+                                                    }
+                                                    disabled={n.val === 0 || this.state.value !== 'addNode'}
                                                 >
-                                                </input>
-                                            </td>
-                                        )
-                                    })}
-                                </tr>
-                            )
-                        })}
+                                                    <input
+                                                        id={`inp-${n.row}-${n.col}`}
+                                                        className="graph-node-input"
+                                                        type="text"
+                                                        value={n.val === 0 ? '' : n.val}
+                                                        onChange={(e) => this.handleValueChange(e, n)}
+                                                        autoFocus
+                                                    >
+                                                    </input>
+                                                </Popup>
+                                            )
+                                        })}
+                                    </tr>
+                                )
+                            })}
                         </tbody>
                     </div>
                 </div>
-                    {graph.edges().map((edge, idx) => {
-                        let d1 = $(`#${edge.v}`)
-                        let d2 = $(`#${edge.w}`)
-                        console.log(d1)
-                        let x1 = d1.offset().left + (d1.width()/2);
-                        let y1 = d1.offset().top + (d1.height()/2);
-                        let x2 = d2.offset().left + (d2.width()/2);
-                        let y2 = d2.offset().top + (d2.height()/2);
-                        return (
-                            <svg key={idx} className="svg">
-                                <line className="svg-line" text="20" id={`line-${edge.v}-${edge.w}`} style={{strokeWidth:"1.5px",stroke:"red"}} x1={x1} y1={y1} x2={x2} y2={y2}></line>
-                                {/* <text x={x3} y={y3} id={`text-${edge[0]}-${edge[1]}-${edge[2]}-${edge[3]}`} font-family="sans-serif"  font-size="12px" fill="black">{edge[4]}</text> */}
-                            </svg>
-                        )
-                    })}
+                {graph.edges().map((edge, idx) => {
+                    let d1 = $(`#${edge.v}`)
+                    let d2 = $(`#${edge.w}`)
+                    console.log(d1)
+                    let x1 = d1.offset().left + (d1.width() / 2);
+                    let y1 = d1.offset().top + (d1.height() / 2);
+                    let x2 = d2.offset().left + (d2.width() / 2);
+                    let y2 = d2.offset().top + (d2.height() / 2);
+                    return (
+                        <svg key={idx} className="svg">
+                            <line className="svg-line" text="20" id={`line-${edge.v}-${edge.w}`} style={{ strokeWidth: "1.5px", stroke: "red" }} x1={x1} y1={y1} x2={x2} y2={y2}></line>
+                            {/* <text x={x3} y={y3} id={`text-${edge[0]}-${edge[1]}-${edge[2]}-${edge[3]}`} font-family="sans-serif"  font-size="12px" fill="black">{edge[4]}</text> */}
+                        </svg>
+                    )
+                })}
             </>
         )
     }
